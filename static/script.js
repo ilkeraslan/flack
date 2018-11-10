8c122acdb12f1eab38d8b0ec3fdd135d0c333792
[Dement0] <ilkeer.ozgur@gmail.com>
2018-11-10 22:19:19 +0100

    Added JavaScript

---
+++ static/script.js
@@ -0,0 +1,42 @@
+// Set localStorage for username
+if (!localStorage.getItem('username'))
+  localStorage.setItem('username', 'guest');
+
+document.addEventListener('DOMContentLoaded', function() {
+
+  // Get localStorage for username
+  document.querySelector('#username').innerHTML = localStorage.getItem('username');
+
+  // By default, submit button is disabled
+  document.querySelector('#username-submit').disabled = true;
+
+  // Enable button only uf there is text in the input field
+  document.querySelector('#username-input').onkeyup = () => {
+    if (document.querySelector('#username-input').value.length > 0)
+      document.querySelector('#username-submit').disabled = false;
+    else
+      document.querySelector('#username-submit').disabled = true;
+  };
+
+  document.querySelector('#username-form').onsubmit = function() {
+    // Create new item for list
+    const li = document.createElement('li');
+    li.innerHTML = "Your user name is: " + document.querySelector('#username-input').value;
+
+    // Set innerHTML of username element
+    let username = document.querySelector('#username-input').value;
+    document.querySelector('#username').innerHTML = username;
+
+    // Set localStorage for username
+    localStorage.setItem('username', username);
+
+    // Append it to users
+    document.querySelector('#users').append(li);
+
+    // Clear input field
+    document.querySelector('#username-input').value = '';
+
+    // Stop form from submitting to other page or website
+    return false;
+  };
+});
